134a25e1fbaa024720caa7db25af0285b3eb60eb
Ajay Kumar <kumarmishraajay45@gmail.com>
2023-02-20 00:02:36 +0530

    Moving State from cart to app

--- src/Cart.js
+++ src/Cart.js
@@ -2,7 +2,10 @@ import React from "react";
 import CartItem from "./CartItem";
 
 // props is used to pass data from parent (cart) to child items (cartItems) with the help of props.props is like attribute to a function
-class Cart extends React.Component{
+/*we are makig this component to functional
+//class Cart extends React.Component{
+  */
+ /* shifting setstate and function to app.js.so that navbar.js and cart.js both csn use state with the help of props.
   constructor(){
     //super is calling constructor of component class for not getting error
     super();
@@ -70,9 +73,12 @@ handleDecreaseQuantity =(product) => {
   this.setState({
     products:items
   })
-  }
-  render(){
-    const {products}= this.state;
+  }  */
+  //render(){
+    //const {products}= this.state;
+    const Cart =(props)=>{
+    const {products}= props;
+
     return (
         <div className="cart">
           {/* first way
@@ -80,17 +86,25 @@ handleDecreaseQuantity =(product) => {
            */}
         {/* second way */}
         {products.map((product)=>{
-         return <CartItem
-        /** here product func,comp,jsx everything is props
+         return (<CartItem
+        /** here product ,func,comp,jsx everything is props
          * these are the name of props which can be anything.
          * KEY is not props .it is for the react so that it cna identify componets.
          */
           product={product}
           key={product.id}
+          /*before shiftig setstate to app.js
           onIncreaseQuantity={this.handleIncreaseQuantity}
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
-
+          */
+         /*After shiftig setstate to app.js
+         props.onIncreaseQuantity here props because in app.js we are passing these function as props and
+         .onIncreaseQuantity instead of .handleIncreaseQuantity because function is handleIncreaseQuantity is in app.js
+         */
+          onIncreaseQuantity={props.onIncreaseQuantity}
+          onDecreaseQuantity={props.onDecreaseQuantity}
+          onDeleteProduct={props.onDeleteProduct}
           /* for demo
           // func={()=>console.log('hey Ajay')}
           // isLoggedin={false}
@@ -98,13 +112,15 @@ handleDecreaseQuantity =(product) => {
           // comp={<CartItem/>}
           */
           /> 
+         )
         })}
         
         </div>
         
     )
   }
-}
+
+//}
 
 
 
